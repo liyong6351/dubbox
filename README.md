f94b500228daf52fa90ed901f09f626d14570c30
liyong <liyong@163.com>
2019-01-05 18:47:41 +0800

    重新初始化

--- README.md
+++
@@ -1,2 +0,0 @@
-# dubbox
-springboot 1.5.8.RELEASE 整合 DUBBOX的一个demo级的项目
--- dubbox-api/src/main/java/com/knight/service/ConService.java
+++
@@ -1,8 +0,0 @@
-package com.knight.service;
-
-import com.knight.entity.Person;
-import com.knight.entity.User;
-
-public interface ConService {
-    Person getPerson(User user);
-}
